ab9fba192aeeba52764d495cd1a9585b85654ba6
Boldbug <boldbug9@gmail.com>
2026-05-30 15:23:40 +0530

    Change weather API location

--- scripts/api.js
+++ scripts/api.js
@@ -2,7 +2,7 @@ import { weatherObjectBuilder } from "./object.js";
 
 
 export async function fetchWeatherFromAPI() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e8f09b47f3f957d8164b5105c509f5f7&units=metric`;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Muzaffarpur,BR,IN&APPID=e8f09b47f3f957d8164b5105c509f5f7&units=metric";
 
     const response = await fetch(url);
 
@@ -12,7 +12,7 @@ export async function fetchWeatherFromAPI() {
     }
     const data = await response.json();
 
-    const weather_data = weatherObjectBuilder("London,UK",data.main.temp,data.main.feels_like,data.wind.speed,data.main.humidity);
+    const weather_data = weatherObjectBuilder("Muzaffarpur , Bihar",data.main.temp,data.main.feels_like,data.wind.speed,data.main.humidity);
 
     console.log(weather_data);
     
