3c9b1169e9b0e33100b7d55781a847c456812904
boldbug <boldbug9@gmail.com>
2026-05-31 21:38:31 +0530

    Implement search

--- scripts/api.js
+++ scripts/api.js
@@ -1,8 +1,8 @@
 import { weatherObjectBuilder } from "./object.js";
 
 
-export async function fetchWeatherFromAPI() {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Muzaffarpur,BR,IN&APPID=e8f09b47f3f957d8164b5105c509f5f7&units=metric";
+export async function fetchWeatherFromAPI(location = "London") {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=e8f09b47f3f957d8164b5105c509f5f7&units=metric`;
 
     const response = await fetch(url);
 
@@ -12,7 +12,7 @@ export async function fetchWeatherFromAPI() {
     }
     const data = await response.json();
 
-    const weather_data = weatherObjectBuilder("Muzaffarpur , Bihar",data.main.temp,data.main.feels_like,data.wind.speed,data.main.humidity);
+    const weather_data = weatherObjectBuilder(location,data.main.temp,data.main.feels_like,data.wind.speed,data.main.humidity);
 
     console.log(weather_data);
     
--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,27 @@
 import {fetchWeatherFromAPI} from './api.js'
-const location = document.getElementById("location");
+const locationCard = document.getElementById("location-card");
 const tempContainer = document.getElementById("temp-container");
 const windSpeedContainer = document.getElementById("wind-speed");
 const humidityLevel = document.getElementById("humidity");
 const feelsLikeContainer = document.getElementById("feels-like");
 const weatherContainer = document.getElementById("weather-container");
+const locationContainer = document.getElementById("location-inpt-container");
+const locationInput = document.getElementById("location-inpt");
 
+renderWeather();
 
-async function renderWeather(){
-    const weatherObj = await fetchWeatherFromAPI();
-
-    location.innerHTML = weatherObj.location;
+async function renderWeather(location){
+    const weatherObj = await fetchWeatherFromAPI(location);
+    locationCard.innerHTML = weatherObj.location;
     tempContainer.innerHTML = weatherObj.tempreature_C;
     windSpeedContainer.innerHTML = weatherObj.windSpeed;
     humidityLevel.innerHTML = weatherObj.humidity;
     feelsLikeContainer.innerHTML = weatherObj.feels_like;
 }
 
-renderWeather();
+locationContainer.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const location = locationInput.value;
+    renderWeather(location);
+
+})
